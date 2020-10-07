6feb1939de9a20bb12afb9c88cfda16a1b1d6f32
Paulo Leite Costa <pauloleitecosta14@gmail.com>
2020-10-07 18:39:32 -0300

    Iniciando projeto

---
+++ src/main/java/com/t2s/conteiner/ConteinerApplication.java
@@ -0,0 +1,13 @@
+package com.t2s.conteiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConteinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConteinerApplication.class, args);
+	}
+
+}
