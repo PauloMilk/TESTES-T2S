0e9d9985dedee42d834a04131aacbddf11479f7c
Paulo Leite Costa <pauloleitecosta14@gmail.com>
2020-10-09 00:12:17 -0300

    Adicionando config dialect mysql

--- src/main/resources/db/migration/V01__CRIAR_TABELA_CONTEINERS.sql
+++ src/main/resources/db/migration/V01__CRIAR_TABELA_CONTEINERS.sql
@@ -1,4 +1,4 @@
-CREATE TABLE if not exists tb_conteiner (
+CREATE TABLE if not exists tb_conteiner(
     cd_conteiner int auto_increment,
     cd_numero varchar(11) not null,
     nm_cliente varchar(200) not null,
